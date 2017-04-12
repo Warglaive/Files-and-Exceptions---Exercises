a3baf8a8c4edca38693a64d9ba4c313961e7a05a
warglaivee <warglaivegm@gmail.com>
2017-04-12 15:58:10 +0300

    01.Filter Extensions written

---
+++ 01.Filter Extensions/01.Filter Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace _01.Filter_Extensions
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            var extension = Console.ReadLine();
+            string[] entry = Directory.GetFiles("input");
+
+            foreach (var file in entry)
+            {
+                if (file.Contains(extension))
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+    }
+}
