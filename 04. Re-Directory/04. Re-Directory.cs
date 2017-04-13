f2a6f82217f618e55f6662f86d36c929767392d0
warglaivee <warglaivegm@gmail.com>
2017-04-14 01:00:44 +0300

    04 Not Working
    05.Products working

---
+++ 04. Re-Directory/04. Re-Directory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _04.Re_Directory
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var input = Directory.GetFiles("input");
+            foreach (var filesx in input)
+            {
+                var extensions = filesx.Split(new[] { '.', '\\' }
+                , StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+                var files = (from file in Directory.EnumerateFiles(filesx)
+                             where extensions.Contains(Path.GetExtension(file), StringComparer.InvariantCultureIgnoreCase) // comment this out if you don't want to filter extensions
+                             select new
+                             {
+                                 Source = file,
+                                 //Destination = Path.Combine(targetPath, Path.GetFileName(file))
+                             });
+            }
+        }
+    }
+}
